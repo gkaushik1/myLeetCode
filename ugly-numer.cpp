b67dfa5792c1e5677b93d2f5bc6837caba913b86
Gopal Kaushik <fanofcricket@gmail.com>
2015-09-13 17:39:05 -0400

    Create ugly-numer.cpp

---
+++ ugly-numer.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/ugly-number/
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num == 1) return true; 
+        if(num <= 0) return false; 
+        
+        while(num > 1)
+        {
+            if(num%2 == 0) num = num/2;
+            else if(num%3 == 0) num = num/3;
+            else if(num%5 == 0) num = num/5; 
+            else if(num > 1) return false;
+        }
+        return true;
+        
+    }
+};
